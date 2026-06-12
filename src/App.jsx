ffee4c9beb192c3986fbfd4e7e2bffcdbb02b665
Sean Thomson <seantomo1680@gmail.com>
2026-06-12 13:03:39 +0200

    Added Macbook canvas and lighting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
 
 const App = () => {
     return (
         <main>
             <Navbar />
             <Hero/>
+            <ProductViewer/>
         </main>
     );
 };
